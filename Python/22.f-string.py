e100421df841729415231a67b29fab9f9318bc1f
Euron.one <145654051+sudhanshukumartech@users.noreply.github.com>
2024-09-02 15:00:09 +0530

    Add files via upload

---
+++ Python/22.f-string.py
@@ -0,0 +1,12 @@
+# s = "My name is {}. I am a {}"
+
+# name = "Bappy"
+# gender = "Male"
+
+# print(s.format(name, gender))
+
+name = "Bappy"
+gender = "Male"
+
+s = f"My name is {name}. I am a {gender}"
+print(s)
